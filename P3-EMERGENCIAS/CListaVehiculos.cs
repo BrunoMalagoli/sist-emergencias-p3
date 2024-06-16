152cfc59786a16b5bd7270f032cda21d3e8cf90a
cristian <toccicristian@hotmail.com>
2024-06-15 21:04:14 -0300

    ordenar vehiculos

--- P3-EMERGENCIAS/CListaVehiculos.cs
+++ P3-EMERGENCIAS/CListaVehiculos.cs
@@ -43,9 +43,41 @@ namespace Emergencias
             TotalVehiculos.AddRange(AutosCollection);
         }
 
+        private void OrdenarListas()
+        {
+            // TotalVehiculos.AddRange(AmbulanciasCollection);
+            // TotalVehiculos.AddRange(AutosCollection);
+            /*
+            Para utilizar este metodo primero hay que llamar al metodo: JuntarListas().
+            */
+            int n = TotalVehiculos.Count;
+
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    
+                    // string patente1 = TotalVehiculos[j].DarPatente();
+                    // string patente2 = TotalVehiculos[j+1].DarPatente();
+                    if (string.Compare( (TotalVehiculos[j] as CVehiculo).DarPatente(),(TotalVehiculos[j+1] as CVehiculo).DarPatente() , StringComparison.Ordinal) > 0)
+                    {
+                        // Intercambiar list[j] y list[j + 1]
+                        object temp = TotalVehiculos[j];
+                        TotalVehiculos[j] = TotalVehiculos[j + 1];
+                        TotalVehiculos[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
         public void MostrarTodosVehiculos()
         {
             JuntarListas();
+            OrdenarListas();
+
+            Console.WriteLine("{0,-12}{1,-12}{2,-12}{3,-12}  ", "PATENTE", "MARCA", "MODELO", "TIPO");
+
             foreach (object vehiculo in TotalVehiculos)
             {
                 if (vehiculo is CAuto)
@@ -64,22 +96,20 @@ namespace Emergencias
         //METODOS PARA TESTEAR
         internal void MostrarListaAutos()
         {
+            Console.WriteLine("{0,-12}{1,-12}{2,-12}  ", "PATENTE", "MARCA", "MODELO");
             foreach (CAuto Autos in AutosCollection)
             {
-                Console.WriteLine(Autos.DarPatente());
-                Console.WriteLine(Autos.DarMarca());
-                Console.WriteLine(Autos.DarModelo());
+                Console.WriteLine("{0,-12}{1,-12}{2,-12}  ", Autos.DarPatente(), Autos.DarMarca(), Autos.DarModelo());
             }
         }
         internal void MostrarListaAmbulancias()
         {
+            Console.WriteLine("{0,-12}{1,-12}{2,-12}{3,-12}  ", "PATENTE", "MARCA", "MODELO","TIPO");
             foreach (CAmbulancia Ambu in AmbulanciasCollection)
             {
-                Console.WriteLine(Ambu.DarTipoAmbulancia());
-                Console.WriteLine(Ambu.DarPatente());
-                Console.WriteLine(Ambu.DarMarca());
-                Console.WriteLine(Ambu.DarModelo());
+                Console.WriteLine("{0,-12}{1,-12}{2,-12}{3,-12}  ", Ambu.DarPatente(), Ambu.DarMarca(), Ambu.DarModelo(),Ambu.DarTipoAmbulancia());
             }
         }
     }
+
 }
